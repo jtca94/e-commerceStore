9511fc569c0ee71e50bde6ccfe69c7e5ffb1e763
jtcannobbio <tomas.cannobbio@gmail.com>
2023-07-16 19:13:59 -0400

    edited homepage card

--- client/src/components/Home/Card.jsx
+++ client/src/components/Home/Card.jsx
@@ -10,7 +10,15 @@ export default function CardHome({product}) {
   const navigate = useNavigate();
   return (
     <Card
-      sx={{maxWidth: 250, my: 2, display:"flex", flexDirection:"column", justifyContent:"space-between", height: "411px"}}
+      sx={{
+        maxWidth: 250,
+        my: 2,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "411px",
+        backgroundColor: "#292929",
+      }}
     >
       <CardActionArea>
         <CardMedia
@@ -19,20 +27,17 @@ export default function CardHome({product}) {
           image={product.image}
           alt={product.name}
         />
-        <CardContent
-          sx={{ display: "block",}}
-        >
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent sx={{display: "block"}}>
+          <Typography color="white" gutterBottom variant="h5" component="div">
             {product.name}
           </Typography>
           <Typography
             sx={{
               display: "inline-block",
-              backgroundColor: "custom.purple",
+              backgroundColor: "#00BB9D",
               color: "white",
               borderRadius: "1rem",
               px: 1.5,
-              py: 0.5,
               mb: 1,
               mr: 1,
             }}
@@ -42,11 +47,10 @@ export default function CardHome({product}) {
           </Typography>
           <Typography
             sx={{
-              backgroundColor: "custom.yellow",
+              backgroundColor: "grey.400",
               borderRadius: "1rem",
               display: "inline-block",
               px: 1.5,
-              py: 0.5,
             }}
             variant="h6"
           >
@@ -54,9 +58,32 @@ export default function CardHome({product}) {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button onClick={() => navigate(`/productos/${product.id}`)} size="small" color="primary">
-          Ver más
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          onClick={() => navigate(`/productos/${product.id}`)}
+          size="small"
+          sx={{
+            color: "white",
+            mr: 1,
+            textDecoration: "underline",
+            textUnderlineOffset: "0.2rem",
+            textUnderlinePosition: "under",
+            fontWeight: "bold",
+            textDecorationColor: "#00ff00",
+            "&:hover": {
+              color: "#00ff00",
+              textDecorationColor: "#00ff00",
+              textDecoration: "underline",
+            },
+          }}
+        >
+          Ver Juego
         </Button>
       </CardActions>
     </Card>
